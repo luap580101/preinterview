6f6c76a450f63824facfdac91e01b06ed7469485
Cheer <84440486+luap580101@users.noreply.github.com>
2025-03-23 20:38:41 +0800

    [feat] 1.安裝 jest  2. 撰寫component 測試

---
+++ preinter-view/__tests__/components/ApplicationCard.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ApplicationCard from "../../components/ApplicationCard";
+import "@testing-library/jest-dom";
+
+const defaultProps = {
+  applicationId: "123",
+  imageUrl: "https://example.com/image.jpg",
+  name: "My Application",
+  contentType: "Game",
+  rating: 4,
+  ratingCount: 100,
+};
+
+describe("ApplicationCard", () => {
+  it("render the component with the correct data", () => {
+    render(<ApplicationCard {...defaultProps} />);
+
+    expect(screen.getByText("My Application")).toBeInTheDocument();
+    expect(screen.getByText("Game")).toBeInTheDocument();
+    expect(screen.getByText("(100)")).toBeInTheDocument();
+    expect(screen.getByAltText("Application Image")).toHaveAttribute(
+      "src",
+      "https://example.com/image.jpg"
+    );
+  });
+});
---
+++ preinter-view/__tests__/components/RecommendationCard.test.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import RecommendationCard from "../../components/RecommendationCard";
+import "@testing-library/jest-dom";
+
+const defaultProps = {
+  imageUrl: "https://example.com/image.jpg",
+  name: "My Recommendation",
+  contentType: "Game",
+};
+
+describe("RecommendationCard", () => {
+  it("render the component with the correct data", () => {
+    render(<RecommendationCard {...defaultProps} />);
+
+    expect(screen.getByText("My Recommendation")).toBeInTheDocument();
+    expect(screen.getByText("Game")).toBeInTheDocument();
+    expect(screen.getByAltText("Recommendation Image")).toHaveAttribute(
+      "src",
+      "https://example.com/image.jpg"
+    );
+  });
+});
